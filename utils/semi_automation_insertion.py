0afa92bd831896f89b677a762727bd0edd072f0e
bavalpreet singh <bavalpreet.singh25@gmail.com>
2021-07-06 09:44:15 +0530

    updated b0 viualsationa and semi_automation_insertion script

--- utils/semi_automation_insertion.py
+++ utils/semi_automation_insertion.py
@@ -1,10 +1,40 @@
 import pandas as pd
 
+#FROM CLIENT INPUT TO PARTICULAR FORMAT
+def pad_dict_list(dict_list, padel):
+    lmax = 0
+    for lname in dict_list.keys():
+        lmax = max(lmax, len(dict_list[lname]))
+    for lname in dict_list.keys():
+        ll = len(dict_list[lname])
+        if  ll < lmax:
+            dict_list[lname] += [padel] * (lmax - ll)
+    return dict_list
 
-def create_rasa_files(path, create_files_path, nlu_file_name, domain_file_name):
+def preparing_intermediate_output_for_nlu_and_domain_filegeneration(path_to_csv, df):
+  intent_list = list(df['intent'].unique())
+  dic = {}
+  for intent in intent_list:
+    variation_list = list(df[df['intent'] == intent]['variations'])
+    question_list = list(set(list(df[df['intent'] == intent]['question'])))
+    print(question_list)
+    dic[intent] = variation_list+question_list
+  padel = 'xyz'
+  pad_dict_list(dic, padel)
+  data = pd.DataFrame(dic)
+  df = data.replace(['xyz'],'')
+  df.to_csv(path_to_csv)
+
+'''
+param: create_files_path - where we want to create the nlu and domain files
+param: nlu_file_name
+param: domain_file_name
+'''
+#GENERATING FILES
+def create_rasa_files(path, create_files_path, nlu_file_name, domain_file_name, Nlu_file_flag = True, Domain_file_flag = True):
     
     #NLU FILE
-    NLU_FILE_CREATION = True
+    NLU_FILE_CREATION = Nlu_file_flag
     if(NLU_FILE_CREATION):
         df = pd.read_csv(r"{}".format(path))
         file = open(create_files_path+'nlu'+nlu_file_name+'.yml',"w")
@@ -19,7 +49,7 @@ def create_rasa_files(path, create_files_path, nlu_file_name, domain_file_name):
 
 
     #DOMAIN FILE
-    DOMAIN_FILE_CREATION = True
+    DOMAIN_FILE_CREATION = Domain_file_flag
     if(DOMAIN_FILE_CREATION):
         file = open(create_files_path+domain_file_name+'.yml',"w")
         file.write("intents:\n")
@@ -36,8 +66,12 @@ def create_rasa_files(path, create_files_path, nlu_file_name, domain_file_name):
     return None
 
 if __name__ == '__main__':
+    path_to_csv = ''
+    df = pd.read_csv('')
+    preparing_intermediate_output_for_nlu_and_domain_filegeneration(path_to_csv)
+    
     path = '/home/bavalpreet/IDP/nlu_sample_format_for_conversion.csv'
-    create_files_path = '/home/bavalpreet/IDP/semiautomation/'
+    create_files_path = '/home/bavalpreet/IDP/generated_data/semiautomation/'
     domain_file_name = '\domain'
     nlu_file_name = '\nlu'
     create_rasa_files(path, create_files_path, nlu_file_name, domain_file_name)

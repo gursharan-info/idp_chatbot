26168f94a9daca7d55a2eb83509e1ece8969de8d
bavalpreet singh <bavalpreet.singh25@gmail.com>
2021-07-13 13:57:37 +0530

    updated framework generation script and nlu-faq-b0

--- utils/paraphraser_quillbot.py
+++ utils/paraphraser_quillbot.py
@@ -5,11 +5,15 @@ import time
 import pandas as pd
 from tqdm import tqdm
 
+
+# preparing string in specific format
 def text_formatter(text):
   text_ = text.split(' ')
   modified_text = '%20'.join(text_)
   return modified_text
 
+
+# hitting api for paraphraased questions
 def paraphrase(text):
   parameters = {'text':text,
                 'strength':'2',
@@ -35,6 +39,8 @@ def paraphrase(text):
   time.sleep(2.5)
   return response.json()
 
+
+# getting sentences from api json response
 def get_sent_from_json(response):
   list_of_data = response['data']
   # print(list_of_data[0].keys())
@@ -48,7 +54,7 @@ if __name__ == "__main__":
   sentences = list(data['questions'].values)
   question = []
   paraphrased_values = []
-  # sent = 'narinder modi is assshole'
+  
   for sent in tqdm(sentences):
     text = text_formatter(sent)
     response = paraphrase(text)

fd38c301d00b6c0b5c02c41160bc9a336551f2f4
sahib <ss9334931@gmail.com>
2021-08-10 14:01:21 +0530

    made changes in action file

--- actions/actions.py
+++ actions/actions.py
@@ -9,6 +9,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 from rasa_sdk.events import SlotSet
+import webbrowser
 class ActionSlotSetter(Action):
 
     def name(self) -> Text:
@@ -25,12 +26,16 @@ class ActionSlotSetter(Action):
              {"payload":'/ok{"intent_button":"faq-dataset"}',"title":"Dataset"}
         ]
 
+            
+
         if tracker.slots['intent_button'] == None:
             print("\n","slots value is ",tracker.slots['intent_button']) 
             dispatcher.utter_message(text="I am there to help you",buttons=buttons)
-        
-        elif str(tracker.latest_message['text']) == str('https://forms.gle/Fk1TxTzAteigKFG87'):
-            dispatcher.utter_message(text='You can fill and submit the Google form')
+
+        # elif 'done' in tracker.slots['intent_button']:
+        #     print(tracker.slots['intent_button'])
+        #     webbrowser.open("https://forms.gle/Fk1TxTzAteigKFG87", new=1)
+        #     dispatcher.utter_message(text="Thanks for your feedback")
 
         else:
             print("\n","Now slots value is ",tracker.slots['intent_button'])  
@@ -39,6 +44,27 @@ class ActionSlotSetter(Action):
 
         return []
 
+class ActionFeedback(Action):
+
+    def name(self) -> Text:
+        return "action_feedback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # buttons = [
+        #    {"payload":'/ok{"intent_button":"done"}',"title":"Feedback"}
+        # {"payload":'https://forms.gle/Fk1TxTzAteigKFG87',"title":"Feedback"}
+        # {"url":'https://forms.gle/Fk1TxTzAteigKFG87',"title":"Feedback-sahib","type": "web_url"}
+        # ]
+        
+        # dispatcher.utter_message(text='please click on feedback button',buttons=buttons)
+        dispatcher.utter_message(text="Please give your feedback")
+        dispatcher.utter_message(text="[feedback](https://forms.gle/Fk1TxTzAteigKFG87)")
+        
+        
+        return [] 
 class ActionVizFaq(Action):
 
     def name(self) -> Text:
@@ -69,65 +95,65 @@ class ActionVizFaq(Action):
 
         # if user message is the form link then don't run further code and odn't return anything
         
-        if str(tracker.latest_message['text']) == str('https://forms.gle/Fk1TxTzAteigKFG87'):
-            dispatcher.utter_message(text='You can fill and submit the Google form ')
-        else:    
-            if tracker.slots['intent_button'] ==None:
-                slot_value_clicked = mapped_intent[tracker.slots['intent_button']]
-            else:
-                slot_value_clicked = tracker.slots['intent_button']
+        # if str(tracker.latest_message['text']) == str('https://forms.gle/Fk1TxTzAteigKFG87'):
+        #     dispatcher.utter_message(text='You can fill and submit the Google form ')
+         
+        if tracker.slots['intent_button'] ==None:
+            slot_value_clicked = mapped_intent[tracker.slots['intent_button']]
+        else:
+            slot_value_clicked = tracker.slots['intent_button']
 
-            # to get intent of user message
-            _intent=tracker.latest_message['intent'].get('name')
-            print("Intent of user message predicted by Rasa ",_intent)
+        # to get intent of user message
+        _intent=tracker.latest_message['intent'].get('name')
+        print("Intent of user message predicted by Rasa ",_intent)
 
-            print(tracker.latest_message['text']) # to get user typed message 
+        print(tracker.latest_message['text']) # to get user typed message 
 
-            intent_found = json.dumps(tracker.latest_message['response_selector'][_intent]['ranking'][0]['intent_response_key'], indent=4)
-            print("retrieval we found (i.e intent response key ) ",intent_found)
+        intent_found = json.dumps(tracker.latest_message['response_selector'][_intent]['ranking'][0]['intent_response_key'], indent=4)
+        print("retrieval we found (i.e intent response key ) ",intent_found)
 
-            # confidence of retrieval intent we found
-            retrieval_intent_confidence = tracker.latest_message['response_selector'][_intent]['response']['confidence']*100
-            
-            print(f"retrieval_intent_confidence we found was {retrieval_intent_confidence}")
-            if str(tracker.latest_message['text']) == str('https://forms.gle/Fk1TxTzAteigKFG87'):
-                dispatcher.utter_message(text='You can fill and submit the Google form')
-
-            elif _intent[:-3] == slot_value_clicked[0] :
-                """ if intent found is same as faq-visualisation or faq-portal or any other category
-                -3 tells we have left - and batch number 
-                ex from faq-visualisation-b0 we took faq-visualisation """
-
-
-            #used eval to remove quotes around the string
-                intent_found = f'utter_{eval(intent_found)}'
-                
-                dispatcher.utter_message(response = intent_found) # use response for defining intent name
+        # confidence of retrieval intent we found
+        retrieval_intent_confidence = tracker.latest_message['response_selector'][_intent]['response']['confidence']*100
         
+        print(f"retrieval_intent_confidence we found was {retrieval_intent_confidence}")
+        if str(tracker.latest_message['text']) == str('https://forms.gle/Fk1TxTzAteigKFG87'):
+            dispatcher.utter_message(text='You can fill and submit the Google form')
+
+        elif _intent[:-3] == slot_value_clicked[0] :
+            """ if intent found is same as faq-visualisation or faq-portal or any other category
+            -3 tells we have left - and batch number 
+            ex from faq-visualisation-b0 we took faq-visualisation """
+
+
+        #used eval to remove quotes around the string
+            intent_found = f'utter_{eval(intent_found)}'
+            
+            dispatcher.utter_message(response = intent_found) # use response for defining intent name
+    
+
+        
+        elif slot_value_clicked == 'No-option':
+            dispatcher.utter_message(text = "Please select any option first",buttons=buttons )
+        
+        else:
+
+            # if retrieval_intent_confidence > 90:
+            intent_found = f'utter_{eval(intent_found)}'
+            
+            dispatcher.utter_message(response = intent_found)
+
+            dispatcher.utter_message(text = f"Seems like you want to ask question from {mapped_intent[ _intent[:-3]]} If yes you are good to go with that  but if you want to ask question from any other category please select a button",buttons=buttons)
+            
+            tracker.slots['intent_button'] = _intent[:-3]
 
             
-            elif slot_value_clicked == 'No-option':
-                dispatcher.utter_message(text = "Please select any option first",buttons=buttons )
+            print(f"Now slot value is {tracker.slots['intent_button']}","\n")
             
-            else:
+            # else: # if confidence is less than 90 percent
+            #     dispatcher.utter_message(text = f"Do you want to ask question from {mapped_intent[ _intent[:-3]]} , If yes please select an options from below"
+            #     ,buttons=buttons)
 
-                # if retrieval_intent_confidence > 90:
-                intent_found = f'utter_{eval(intent_found)}'
-                
-                dispatcher.utter_message(response = intent_found)
-
-                dispatcher.utter_message(text = f"Seems like you want to ask question from {mapped_intent[ _intent[:-3]]} If yes you are good to go with that  but if you want to ask question from any other category please select a button",buttons=buttons)
-                
-                tracker.slots['intent_button'] = _intent[:-3]
-
-                
-                print(f"Now slot value is {tracker.slots['intent_button']}","\n")
-                
-                # else: # if confidence is less than 90 percent
-                #     dispatcher.utter_message(text = f"Do you want to ask question from {mapped_intent[ _intent[:-3]]} , If yes please select an options from below"
-                #     ,buttons=buttons)
-
-            return [SlotSet(key = "intent_button", value= [str(_intent[:-3])] ) ] # setting slot values
-      
+        return [SlotSet(key = "intent_button", value= [str(_intent[:-3])] ) ] # setting slot values
+    
 
 
